89f9439fc1fa7852a6de6f3b88158102044d8a4a
George Antonious <george@antonious.ca>
2017-10-26 22:45:05 -0600

    Add fullscreen activity

---
+++ app/src/main/java/ca/antonious/gfys/FullScreenGifActivity.java
@@ -0,0 +1,32 @@
+package ca.antonious.gfys;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class FullScreenGifActivity extends AppCompatActivity {
+    private static String EXTRA_GIF_URL = "FullScreenGifActivity.GifUrl";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_screen_gif);
+
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+
+        Glide.with(this)
+                .asGif()
+                .load(getIntent().getStringExtra(EXTRA_GIF_URL))
+                .into(imageView);
+    }
+
+    public static void start(Context from, String gifUrl) {
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_GIF_URL, gifUrl);
+        from.startActivity(intent);
+    }
+}
